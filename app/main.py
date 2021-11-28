bfd74145e765668bfb3b11aa03f5bb099ce97b74
as1mple <piterone.sqaure@gmail.com>
2021-11-28 18:00:05 +0200

    Enable CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 from app.modules.handlers import router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 def get_application() -> FastAPI:
     application = FastAPI()
+    origins = ["*"]
+
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     application.include_router(router)
+
     return application
 
 
